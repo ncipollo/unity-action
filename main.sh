a7ef134709f61fc3b13316641512464570eb3d85
Nick Cipollo <njc1115@gmail.com>
2019-09-07 13:08:10 -0400

    Fix variable name

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo 'attempting to run unity!'
-echo $username
+echo $INPUT_USERNAME
+echo 'there should be a user name...'
 
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
 /opt/Unity/Editor/Unity \
 -logFile /dev/stdout \
 -batchmode \
--username "$username" -password "$password"
+-username "$INPUT_USERNAME" -password "$INPUT_PASSWORD"
