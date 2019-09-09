d86f2d6e377f6bd201c3b1cc27d2c1164c5c9976
Nick Cipollo <njc1115@gmail.com>
2019-09-08 22:29:17 -0400

    Attempt another thing

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
-echo 'Writing license to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
-echo "$INPUT_LICENSE" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+# echo 'Writing license to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
+# echo "$INPUT_LICENSE" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+
+cp $GITHUB_WORKSPACE/Unity_v2019.ulf /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
 set -x
 
