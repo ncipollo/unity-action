2a4f1a7e6d9372847d262707443b1ed1baccd3e2
Nick Cipollo <njc1115@gmail.com>
2019-09-15 18:01:33 -0400

    Fix main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,8 @@
 if [ -z "$INPUT_LICENSE" ]; then
       sh license.sh
 else
-      echo "$INPUT_MODE"
       sh setup.sh
-      if [ "$INPUT_MODE" == build ]; then
+      if [ "$INPUT_MODE" == "build" ]; then
             echo "would run build here"
       else 
             sh test.sh
