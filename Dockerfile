410f81bf21bebaa89e785f149a571559097ed7e4
Nick Cipollo <njc1115@gmail.com>
2019-09-15 18:18:16 -0400

    Move away from sh

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM gableroux/unity3d:2019.2.2f1
 WORKDIR /root
 COPY . .
 
-CMD cd /root && sh main.sh
+CMD cd /root && ./main.sh
--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -z "$INPUT_LICENSE" ]; then
-      sh license.sh
+       ./license.sh
 else
-      sh setup.sh
+      ./setup.sh
       if [ "$INPUT_MODE" == "build" ]; then
             echo "would run build here"
       else 
-            sh test.sh
+            ./test.sh
       fi
 fi
