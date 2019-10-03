d93d141727cd5e7da83785d6f6bb8ed30cb9200f
Nick Cipollo <njc1115@gmail.com>
2019-10-02 20:08:17 -0400

    Add support for serial number

--- license.sh
+++ license.sh
@@ -4,11 +4,13 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
 /opt/Unity/Editor/Unity \
 -logFile /dev/stdout \
 -batchmode \
--username "$INPUT_USERNAME" -password "$INPUT_PASSWORD" \
+-username "$INPUT_USERNAME" \
+-password "$INPUT_PASSWORD" \
+${INPUT_SERIAL:+'-serial '"$INPUT_SERIAL"} \
 || true
 
 echo '\n\n================================================================================'
-echo 'No license provided! Assuming one needs to be generated for unity personal...'
+echo 'No license provided! Assuming one needs to be generated...'
 echo 'Step1: Look for xml dump in output above'
 echo 'Step2: Copy this into a file called unity3d.alf'
 echo 'Step3: Upload this file to https://license.unity3d.com/manual'
